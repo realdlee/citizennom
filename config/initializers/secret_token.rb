7b74c807eda618a34f2dffcd4a7e8f20828ee0ca
David Lee <ambitiouspear@gmail.com>
2013-03-18 14:32:29 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citizennom::Application.config.secret_token = '45e4638106825c3d6bdded4911ac2fc321619f7d178bf8c8f6c5fa81cb88d4da0bc385106ef6bd2f0afd944714fceb0fffa67778eeebf302296751cc9dbecead'
